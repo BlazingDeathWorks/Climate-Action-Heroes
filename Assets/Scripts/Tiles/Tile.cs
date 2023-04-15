8c84602b3394380106b5504d76ca31677ea2b262
Jason Chan <shunsei.chan@gmail.com>
2023-04-15 11:30:17 -0700

    Tiles Folder Structure Complete

--- Assets/Scripts/Tiles/Tile.cs
+++ Assets/Scripts/Tiles/Tile.cs
@@ -2,12 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tile : MonoBehaviour
+namespace DonutStudios.Tiles
 {
-    // Start is called before the first frame update
-    void Start()
+    internal class Tile : MonoBehaviour
     {
-        Destroy(gameObject);
-        Debug.Log("Hello");
+
     }
 }
