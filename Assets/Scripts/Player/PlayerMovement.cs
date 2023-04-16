77ac4c47640823f2fd7a3cd62d236a71f75cb3bc
Jason Chan <shunsei.chan@gmail.com>
2023-04-16 15:59:52 -0700

    Player Movement Complete

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+internal class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed = 1;
+    private float _x, _y;
+    private Rigidbody2D _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _x = Input.GetAxisRaw("Horizontal");
+        _y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = new Vector2(_x, _y).normalized * _speed;
+    }
+}
