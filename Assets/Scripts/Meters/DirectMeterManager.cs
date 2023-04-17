a90b60774e2c697641a5666c1fef1f53dbeb3bb0
Jason Chan <shunsei.chan@gmail.com>
2023-04-16 23:37:15 -0700

    Home

--- Assets/Scripts/Meters/DirectMeterManager.cs
+++ Assets/Scripts/Meters/DirectMeterManager.cs
@@ -2,29 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using FutureInspireGames.Singleton;
 
 namespace DonutStudios.Meters
 {
-    internal class DirectMeterManager : MonoBehaviour
+    internal class DirectMeterManager : Singleton<DirectMeterManager>
     {
+        public float HungerLevel { get; set; } 
+        public float ThirstLevel { get; set;}
         [SerializeField] private float _decreaseSpeed = 1;
         [SerializeField] private Slider _hungerBar;
         [SerializeField] private Slider _thirstBar;
-        private float _hungerLevel, _thirstLevel;
 
-        private void Awake()
+        protected override void Awake()
         {
-            _hungerLevel = _hungerBar.maxValue;
-            _thirstLevel = _thirstBar.maxValue;
+            base.Awake();
+            HungerLevel = _hungerBar.maxValue;
+            ThirstLevel = _thirstBar.maxValue;
         }
 
         private void Update()
         {
-            _hungerLevel -= _decreaseSpeed * Time.deltaTime;
-            _thirstLevel -= _decreaseSpeed * Time.deltaTime;
+            HungerLevel -= _decreaseSpeed * Time.deltaTime;
+            ThirstLevel -= _decreaseSpeed * Time.deltaTime;
 
-            _hungerBar.value = _hungerLevel;
-            _thirstBar.value = _thirstLevel;
+            _hungerBar.value = HungerLevel;
+            _thirstBar.value = ThirstLevel;
+        }
+
+        public void IncrementDecreaseSpeed(float value)
+        {
+            _decreaseSpeed += value;
         }
     }
 }
---
+++ Assets/Scripts/Meters/Home.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DonutStudios.Meters
+{
+    internal class Home : MonoBehaviour
+    {
+        [SerializeField] private float _humanSpawnRate = 180;
+        [SerializeField] private float _decreaseIncrement = 0.5f;
+        private float _timeSinceLastHumanSpawned;
+
+        private void Update()
+        {
+            _timeSinceLastHumanSpawned += Time.deltaTime;
+
+            if (_timeSinceLastHumanSpawned >= _humanSpawnRate)
+            {
+                _timeSinceLastHumanSpawned = 0;
+                DirectMeterManager.Instance.IncrementDecreaseSpeed(_decreaseIncrement);
+            }
+        }
+    }
+}
